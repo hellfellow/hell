f95031feea68249225f447f4f8e0721fa3e9252c
hellfellow <49310546+hellfellow@users.noreply.github.com>
2019-04-09 19:31:43 +0300

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,14 +1,7 @@
-anime({
-  targets: '.staggering-grid-demo .el',
-  scale: [
-    {value: .1, easing: 'easeOutSine', duration: 500},
-    {value: 1, easing: 'easeInOutQuad', duration: 1200}
-  ],
-  delay: anime.stagger(200, {grid: [14, 5], from: 'center'})
-});
-
 
 var navbar=document.querySelector('header')
 var body = document.querySelector('body')
 var scrollValue=body.scrollTop
-if (scrollValue>0) {navbar.classList.add('shadow')} 
+
+
+body.addEventListener('scroll',(){if (scrollValue>0) {navbar.classList.add('shadow')} })
